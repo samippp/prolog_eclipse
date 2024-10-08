c11776d2e6c8ae0c964e9d47b04a671ffbbd6d6f
f-roze <ferozn.89@gmail.com>
2024-10-08 18:13:06 -0400

    added names and student no.

--- assignment2/q5_robocup.pl
+++ assignment2/q5_robocup.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Sami Peng 501163935
+%%%%% NAME: Feroz Noormohamed 500203723
+%%%%% NAME: Mohammad Abdul Rahman 500961636
 %
 % Add the required rules in the corresponding sections. 
 % If you put the rules in the wrong sections, you will lose marks.
