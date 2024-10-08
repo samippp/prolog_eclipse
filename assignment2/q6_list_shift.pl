d7b454b7b217b56f83d4261aee2b897f21c02984
Mohammad Siddiqui <96317093+marzy13@users.noreply.github.com>
2024-10-08 13:36:03 -0400

    Update q6_list_shift.pl

--- assignment2/q6_list_shift.pl
+++ assignment2/q6_list_shift.pl
@@ -16,24 +16,24 @@
 %%%%% Put your rules for listShift and any helper predicates below
 
 listShift(List, V, Result) :-
-    length_list(List, Length),
+    getLength(List, Length),
     Shift is V mod Length,
-    shift_by(List, Shift, Result).
+    shiftElements(List, Shift, Result).
 
-shift_by(Result, 0, Result).
-shift_by(next(H, T), V, Result) :-
+shiftElements(Result, 0, Result).
+shiftElements(next(H, T), V, Result) :-
     V > 0,
-    append_to_end(T, H, TempList),
+    moveToEnd(T, H, TempList),
     V1 is V - 1,
-    shift_by(TempList, V1, Result).
+    shiftElements(TempList, V1, Result).
 
 
-append_to_end(nil, Elem, next(Elem, nil)).
-append_to_end(next(H, T), Elem, next(H, NewTail)) :-
-    append_to_end(T, Elem, NewTail).
+moveToEnd(nil, Elem, next(Elem, nil)).
+moveToEnd(next(H, T), Elem, next(H, NewTail)) :-
+    moveToEnd(T, Elem, NewTail).
 
-
-length_list(nil, 0).
-length_list(next(_, T), Length) :-
-    length_list(T, L1),
+getLength(nil, 0).
+getLength(next(_, T), Length) :-
+    getLength(T, L1),
     Length is L1 + 1.
+
