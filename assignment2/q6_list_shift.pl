ee8d8f87fa0d35bf09506bab03c462c7720df9f0
Mohammad Siddiqui <96317093+marzy13@users.noreply.github.com>
2024-10-08 13:27:46 -0400

    Update q6_list_shift.pl

--- assignment2/q6_list_shift.pl
+++ assignment2/q6_list_shift.pl
@@ -15,3 +15,25 @@
 %%%%% SECTION: listShift
 %%%%% Put your rules for listShift and any helper predicates below
 
+listShift(List, V, Result) :-
+    length_list(List, Length),
+    Shift is V mod Length,
+    shift_by(List, Shift, Result).
+
+shift_by(Result, 0, Result).
+shift_by(next(H, T), V, Result) :-
+    V > 0,
+    append_to_end(T, H, TempList),
+    V1 is V - 1,
+    shift_by(TempList, V1, Result).
+
+
+append_to_end(nil, Elem, next(Elem, nil)).
+append_to_end(next(H, T), Elem, next(H, NewTail)) :-
+    append_to_end(T, Elem, NewTail).
+
+
+length_list(nil, 0).
+length_list(next(_, T), Length) :-
+    length_list(T, L1),
+    Length is L1 + 1.
